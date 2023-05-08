315330fd067be940c407d265de12ecd7f2e720b9
Putumafrizal <130467401+Putumafrizal@users.noreply.github.com>
2023-05-09 01:32:51 +0700

    10120911Latihan2
    PutuMafrizal

---
+++ app/src/main/java/com/example/lat2if10k10120917ristianaditya/HomeActivity.kt
@@ -0,0 +1,18 @@
+//Nim   : 10120911
+//Nama  : Putu Mafrizal Alif Astika
+//Kelas : IF10k
+//TGL   : 30 04 2023
+
+package com.example.lat2if10k10120911
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        supportActionBar?.hide();
+    }
+}
